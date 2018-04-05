d85e825ee1fff72a6fe4b7a3a307d03444ee8135
lesia <lesia.lin@gmail.com>
2018-04-05 21:46:08 +0800

    finish kalman filter update and predict code

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,7 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -21,17 +21,22 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  //predict state
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  //update the state by using Kalman Filter equations
+  VectorXd y = z - H_ * x_;
+  MatrixXd H_t = H_.transpose();
+  MatrixXd S = H_ * P_ * H_t + R_;
+  MatrixXd K = P_ * H_t * S.inverse();
+  //new state
+  x_ = x_ + (K * y);
+  MatrixXd I =  MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - K * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
